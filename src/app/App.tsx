b04c5d348a5c1c6d0308a28147c80aa8db3a9259
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2025-01-19 11:03:25 +0300

    refactor: delete unnecessary Emitter call from App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,8 @@
 import { ErrorBoundary } from '@/app/ErrorBoundary/ErrorBoundary';
 import { CardsList } from '@/features/ui/CardsList';
 import { Layout } from '@/app/layout/Layout';
-import { EventEmitter } from '@/emitter';
 
 function App() {
-  const emitter = new EventEmitter<{ message: string }>();
-  const logData = (data: { message: string }) => console.log(data.message);
-  emitter.on('data', logData);
-  emitter.emit('data', { message: 'Hello world' });
-
-  // emitter.off('data', logData);
-
-  // emitter.emit('data', { message: 'Something go wrong' });
   return (
     <Layout>
       <ErrorBoundary>
